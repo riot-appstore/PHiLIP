932baea29a7c8bdb60d868f7d212136198b5f13e
MrKevinWeiss <weiss.kevin604@gmail.com>
2020-03-11 09:17:18 +0100

    cleanup(fw): Move HAL_MspInit to port

--- FW/Src/port.c
+++ FW/Src/port.c
@@ -33,6 +33,32 @@ I2C_HandleTypeDef hi2c_dut;
 TIM_HandleTypeDef htim_ic;
 TIM_HandleTypeDef htim_pwm;
 
+
+/**
+ * Initializes the Global MSP.
+ */
+void HAL_MspInit(void) {
+	__HAL_RCC_AFIO_CLK_ENABLE();
+	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+
+	/* MemoryManagement_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(MemoryManagement_IRQn, 0, 0);
+	/* BusFault_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(BusFault_IRQn, 0, 0);
+	/* UsageFault_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(UsageFault_IRQn, 0, 0);
+	/* SVCall_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(SVCall_IRQn, 0, 0);
+	/* DebugMonitor_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(DebugMonitor_IRQn, 0, 0);
+	/* PendSV_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(PendSV_IRQn, 0, 0);
+	/* SysTick_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(SysTick_IRQn, DEFAULT_INT_PRIO, 0);
+
+	__HAL_AFIO_REMAP_SWJ_NOJTAG();
+}
+
 #ifdef BLUEPILL
 
 /* TIM1 init function */
--- FW/Src/stm32f1xx_hal_msp.c
+++ FW/Src/stm32f1xx_hal_msp.c
@@ -51,40 +51,6 @@
 
 #include "port.h"
 
-/**
- * Initializes the Global MSP.
- */
-void HAL_MspInit(void) {
-	/* USER CODE BEGIN MspInit 0 */
-
-	/* USER CODE END MspInit 0 */
-
-	__HAL_RCC_AFIO_CLK_ENABLE()
-	;
-
-	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
-
-	/* System interrupt init*/
-	/* MemoryManagement_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(MemoryManagement_IRQn, 0, 0);
-	/* BusFault_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(BusFault_IRQn, 0, 0);
-	/* UsageFault_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(UsageFault_IRQn, 0, 0);
-	/* SVCall_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(SVCall_IRQn, 0, 0);
-	/* DebugMonitor_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(DebugMonitor_IRQn, 0, 0);
-	/* PendSV_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(PendSV_IRQn, 0, 0);
-	/* SysTick_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(SysTick_IRQn, DEFAULT_INT_PRIO, 0);
-
-	/**NOJTAG: JTAG-DP Disabled and SW-DP Enabled
-	 */
-	__HAL_AFIO_REMAP_SWJ_NOJTAG();
-}
-
 void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c) {
 
 	GPIO_InitTypeDef GPIO_InitStruct;
