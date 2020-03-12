dce068faafc6e8d73557e154ec65af92a1b06c80
MrKevinWeiss <weiss.kevin604@gmail.com>
2020-03-12 10:22:24 +0100

    cleanup(fw/map_if): Cleanup map if module

--- FW/Src/map_if.c
+++ FW/Src/map_if.c
@@ -19,7 +19,7 @@
  ******************************************************************************
  */
 
-/* Includes ------------------------------------------------------------------*/
+/* Includes *******************************************************************/
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -27,24 +27,16 @@
 
 #include "PHiLIP_typedef.h"
 #include "PHiLIP_map.h"
-#include "app_errno.h"
 #include "app_reg.h"
 #include "map_if.h"
 
-/* Private defines -----------------------------------------------------------*/
+/* Private macros *************************************************************/
 /** @brief Macro for getting the type size from the map */
 #define T_SIZE(x) (PHILIP_TYPE_SIZE[MAP_T_TYPE[x]])
 
-/* Functions -----------------------------------------------------------------*/
-/**
- * @brief Gets parameters of a record in the memory map.
- *
- * @param[in]	index		index of the record to get
- * @param[out]	str			string to put the record information
- *
- * @return 		EOK on success
- * @return 		EOVERFLOW invalid record index
- */
+/******************************************************************************/
+/*           Functions                                                        */
+/******************************************************************************/
 error_t get_mm(uint16_t index, char* str) {
 	if (index >= MAP_T_NUM_OF_RECORDS) {
 		return EOVERFLOW;
@@ -55,20 +47,9 @@ array_size\":%d,\"bit_offset\":%d,\"bits\":%d,\"result\":0}\n",
 			MAP_T_NAME[index], MAP_T_OFFSET[index],
 			PHILIP_TYPE_NAME[MAP_T_TYPE[index]], MAP_T_ARRAY_SIZE[index],
 			MAP_T_BIT_OFFSET[index], MAP_T_BITS[index]);
-	return EOK;
+	return 0;
 }
 
-/**
- * @brief Gets the value of a record in the memory map
- *
- * @param[in]	mm_name		name of the record
- * @param[in]	array_index	index if the record is an array
- * @param[out]	data		data from the record
- *
- * @return 		EOK on success
- * @return 		EINVAL Invalid value
- * @return 		ERANGE invalid number range of array
- */
 error_t get_mm_val(char* mm_name, uint32_t array_index, uint32_t *data) {
 	int i;
 	error_t err = EINVAL;
@@ -82,12 +63,12 @@ error_t get_mm_val(char* mm_name, uint32_t array_index, uint32_t *data) {
 						(uint8_t*) data, T_SIZE(i));
 			} else if (MAP_T_BITS[i]) {
 				err = read_regs(MAP_T_OFFSET[i], (uint8_t*) data, T_SIZE(i));
-				if (err != EOK) {
+				if (err != 0) {
 					return err;
 				}
 				*data = (*data) >> MAP_T_BIT_OFFSET[i];
 				*data = (*data) & ((1 << MAP_T_BITS[i]) - 1);
-				return EOK;
+				return 0;
 			}
 			return read_regs(MAP_T_OFFSET[i], (uint8_t*) data, T_SIZE(i));
 		}
@@ -95,18 +76,6 @@ error_t get_mm_val(char* mm_name, uint32_t array_index, uint32_t *data) {
 	return err;
 }
 
-/**
- * @brief Sets the value of a record in the memory map
- *
- * @param[in]	mm_name		name of the record
- * @param[in]	array_index	index if the record is an array
- * @param[in]	data		data for the record
- * @param[in]	access		access level of the calling function
- *
- * @return 		EOK on success
- * @return 		EINVAL Invalid value
- * @return 		ERANGE invalid number range of array
- */
 error_t set_mm_val(char* mm_name, uint32_t array_index, uint32_t data,
 		uint8_t access) {
 	int i;
@@ -122,7 +91,7 @@ error_t set_mm_val(char* mm_name, uint32_t array_index, uint32_t data,
 			} else if (MAP_T_BITS[i]) {
 				uint32_t tmp = 0;
 				err = read_regs(MAP_T_OFFSET[i], (uint8_t*) &tmp, T_SIZE(i));
-				if (err != EOK) {
+				if (err != 0) {
 					return err;
 				}
 				data &= ((1 << MAP_T_BITS[i]) - 1);
